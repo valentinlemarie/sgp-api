2257e9b78bccdcd210e60850b20ef164efcec58c
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-06-01 15:04:38 +0200

    correction bd héroku

--- src/main/java/com/example/demo/listener/StartUpAppListener.java
+++ src/main/java/com/example/demo/listener/StartUpAppListener.java
@@ -15,26 +15,33 @@ import com.example.demo.repository.DepartementRepository;
 @Component
 public class StartUpAppListener {
 
-    @Autowired CollaborateursRepository collaborateurs;
-    @Autowired DepartementRepository departements;
-    @Autowired BanqueRepository banques;
-    @EventListener(ContextRefreshedEvent.class)
-    public void contextRefreshedEvent() {
-        // capture du démarrage de l'application
-        // à un moment où le contexte Spring est complètement créé
-        
-    	banques.save(new Banque("LaPoste","123","1234"));
-    	banques.save(new Banque("CreditLoyonais","456","4567"));
-    	banques.save(new Banque("SocieteGenerale","789","7890"));
-    	
-    
-    	
-    	departements.save(new Departement("informatique"));
-    	departements.save(new Departement("commercial"));
-    	departements.save(new Departement("RessourceHuumaine"));
-    	
-    	collaborateurs.save(new Collaborateurs("LMV", "LEMARIE", "Valentin",banques.getOne(1) ,departements.getOne(1)));
-    	collaborateurs.save(new Collaborateurs("TG", "TOURRILHES", "Géraud",banques.getOne(2) ,departements.getOne(2)));
-    	collaborateurs.save(new Collaborateurs("LMB", "LEMARIE", "Benjamin",banques.getOne(3) ,departements.getOne(3)));
-    }
+	@Autowired
+	CollaborateursRepository collaborateurs;
+	@Autowired
+	DepartementRepository departements;
+	@Autowired
+	BanqueRepository banques;
+
+	@EventListener(ContextRefreshedEvent.class)
+	public void contextRefreshedEvent() {
+		// capture du démarrage de l'application
+		// à un moment où le contexte Spring est complètement créé
+		if (collaborateurs.count() + departements.count() + banques.count() == 0) {
+			banques.save(new Banque("LaPoste", "123", "1234"));
+
+			banques.save(new Banque("CreditLoyonais", "456", "4567"));
+			banques.save(new Banque("SocieteGenerale", "789", "7890"));
+
+			departements.save(new Departement("informatique"));
+			departements.save(new Departement("commercial"));
+			departements.save(new Departement("RessourceHuumaine"));
+
+			collaborateurs
+					.save(new Collaborateurs("LMV", "LEMARIE", "Valentin", banques.getOne(1), departements.getOne(1)));
+			collaborateurs
+					.save(new Collaborateurs("TG", "TOURRILHES", "Géraud", banques.getOne(2), departements.getOne(2)));
+			collaborateurs
+					.save(new Collaborateurs("LMB", "LEMARIE", "Benjamin", banques.getOne(3), departements.getOne(3)));
+		}
+	}
 }
